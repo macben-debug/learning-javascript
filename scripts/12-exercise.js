a1f4324fe202c970e8bda15380655996cfbd1bcf
macben-debug <duyilemitemiloluwa@gmail.com>
2026-03-27 13:04:42 +0000

    exercise 12

--- scripts/12-exercise.js
+++ scripts/12-exercise.js
@@ -1,15 +1,40 @@
-const add = function(){
+let add = function(){
     console.log(2+3);
 }
 
-let i =0;
+/*let i =0;
 while(i<2){
     add();
     i++;
-}
+}*/
 
 function runTwice(fun){
-    for (let i =0; i <2; i++){
-        console.log()
-    }
+    fun();
+    fun();
+}
+
+runTwice(add);  
+
+runTwice(function(){
+    console.log('12b');
+});
+
+
+
+function buttonChange(){
+    let button = document.querySelector('.js-button');
+    button.innerHTML ='Loading...';
+   setTimeout (function (){
+    button.innerHTML ='Finished!';
+   },2000)
+}
+
+function addedButton(){
+    let timeoutId; 
+    const display = document.querySelector('.js-display');
+    display.innerHTML = 'Added';
+    clearTimeout(timeoutId)
+    timeoutId =setTimeout(function (){
+        display.innerHTML ='';
+    },2000)
 }
