470c370895e2d7f022246316ff08d5182df656f5
macben-debug <duyilemitemiloluwa@gmail.com>
2026-03-27 21:11:10 +0000

    finish the exercise 12 later

--- scripts/12-exercise.js
+++ scripts/12-exercise.js
@@ -24,7 +24,7 @@ runTwice(function(){
 function buttonChange(){
     let button = document.querySelector('.js-button');
     button.innerHTML ='Loading...';
-   setTimeout (function (){
+   setTimeout ( ()=>{
     button.innerHTML ='Finished!';
    },2000)
 }
@@ -34,7 +34,18 @@ function addedButton(){
     const display = document.querySelector('.js-display');
     display.innerHTML = 'Added';
     clearTimeout(timeoutId)
-    timeoutId =setTimeout(function (){
+    timeoutId =setTimeout( () =>{
         display.innerHTML ='';
     },2000)
+}
+let messages =2;
+while (messages>0){
+    setInterval(function(){
+    if (document.title === '12 Exercises'){
+    document.title= `(${messages}) New messages`;
+    }
+    else{
+    document.title= '12 Exercises';
+    }
+    }, 1000);
 }
